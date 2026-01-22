dfe800be1c336bdfd31f67498ce47429920560b6
Ivan <invoker775577@gmail.com>
2026-01-22 18:30:21 +0300

    Add .env configuration and requirements

--- services.py
+++ services.py
@@ -1,5 +1,9 @@
 # services.py
-# Здесь живет бизнес-логика. 
+import os
+from dotenv import load_dotenv
+
+# Загружаем секреты из файла .env
+load_dotenv()
 
 class InventoryService:
     def check_stock(self, product, quantity):
@@ -15,14 +19,21 @@ class InventoryService:
 
 class PaymentService:
     def process_payment(self, user, amount):
+        # Читаем секретный ключ из .env (имитация проверки безопасности)
+        api_key = os.getenv("BANK_API_KEY")
+        
+        print(f"🔒 (Bank System connect with Key: {api_key}...)") # Для наглядности
+        
         if user.balance >= amount:
             user.balance -= amount
             print(f"💸 ОПЛАТА: Списано {amount}$. У {user.name} осталось {user.balance}$")
             return True
         else:
-            print(f"❌ ОПЛАТА: Недостаточно средств! Нужно {amount}, а есть {user.balance}")
+            print(f"❌ ОПЛАТА: Недостаточно средств!")
             return False
 
 class NotificationService:
     def send_sms(self, user, message):
-        print(f"📩 SMS для {user.name} ({user.email}): {message}")
+        # Читаем email отправителя из настроек
+        sender = os.getenv("SMS_SENDER_EMAIL")
+        print(f"📩 SMS от {sender} для {user.name}: {message}")
